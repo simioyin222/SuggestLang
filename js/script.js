068b85dafc0820011ce02ec3bc9035becc369c89
Simi O <coolkid1984@ymail.com>
2023-10-20 17:07:38 -0500

    add remove of console log and add the rest of id retrevials

--- js/script.js
+++ js/script.js
@@ -3,6 +3,10 @@ document.getElementById("guessForm").addEventListener("submit", function(event) 
 
   const experience = parseInt(document.getElementById("experience").value);
   const favProject = document.getElementById("favProject").value;
-  console.log("Experience:", experience);
-  console.log("Favorite Project:", favProject);
+  const favMagic = document.getElementById("favMagic").value;
+  const favPersonality = document.getElementById("favPersonality").value;
+  const favSuperpower = document.getElementById("favSuperpower").value;
+  const favDrink = document.getElementById("favDrink").value;
+
+  
 })
