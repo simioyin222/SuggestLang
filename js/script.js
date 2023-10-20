a5ee6a88bd7c780f757a8172c4c91bf0de96d05c
Simi O <coolkid1984@ymail.com>
2023-10-20 17:03:53 -0500

    add calling of id to be retrived in js and console log to test

--- js/script.js
+++ js/script.js
@@ -1,3 +1,8 @@
 document.getElementById("guessForm").addEventListener("submit", function(event) {
   event.preventDefault();
+
+  const experience = parseInt(document.getElementById("experience").value);
+  const favProject = document.getElementById("favProject").value;
+  console.log("Experience:", experience);
+  console.log("Favorite Project:", favProject);
 })
